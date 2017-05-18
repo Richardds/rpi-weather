cf939bbe57ab50a4d847d9b5fed3e506e610ea80
Richard Boldiš <boldisrichard@gmail.com>
2017-05-18 22:37:30 +0200

    Changing rotation of horizontal labels

--- resources/views/graphs.blade.php
+++ resources/views/graphs.blade.php
@@ -5,6 +5,23 @@
     <title>Raspberry Pi Weather Station</title>
     <link href="{{ url("/assets/css/chartist.min.css") }}" type="text/css" rel="stylesheet">
     <style>
+        .chart svg.ct-chart-bar, .chart svg.ct-chart-line {
+            overflow: visible;
+        }
+
+        .chart .ct-label.ct-horizontal.ct-end {
+            position: relative;
+            -webkit-box-pack: end;
+            -ms-flex-pack: end;
+            justify-content: flex-end;
+            text-align: right;
+            -webkit-transform-origin: 100% 0;
+            transform-origin: 100% 0;
+            -webkit-transform: translate(-100%) rotate(-45deg);
+            transform: translate(-100%) rotate(-45deg);
+            white-space: nowrap;
+        }
+
         #chart-temperature .ct-line {
             stroke: rgba(255, 200, 0, 0.8);
         }
@@ -38,8 +55,8 @@
 <body>
 
 <h1 class="title">Raspberry Pi weather station</h1>
-<div id="chart-temperature"></div>
-<div id="chart-humidity"></div>
+<div id="chart-temperature" class="chart"></div>
+<div id="chart-humidity" class="chart"></div>
 
 <script src="{{ url("/assets/js/chartist.min.js") }}" type="application/javascript"></script>
 <script src="{{ url("/assets/js/rpi-weather.js") }}" type="application/javascript"></script>
